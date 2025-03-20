f0c5fe5b812884a376d9f41d1a481b9eb9588dee
GiriManas <giri.manas007@gmail.com>
2025-03-20 21:35:13 +0530

    Update dashboard.tsx

--- dashboard.tsx
+++ dashboard.tsx
@@ -1,8 +1,12 @@
-  <Link href={item.link} key={item.id} className="w-[280px]">
-            <Card className="shadow-md transition-transform hover:scale-105 cursor-pointer">
-              <CardHeader>
-                <CardTitle>{item.name}</CardTitle>
-                <CardDescription>{item.description}</CardDescription>
-              </CardHeader>
-            </Card>
-          </Link>
+<div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+  {DISPLAY_APPS.map((item) => (
+    <Link href={item.link} key={item.id} className="w-full">
+      <Card className="w-[280px] h-[150px] shadow-md transition-transform hover:scale-105 cursor-pointer flex flex-col justify-center">
+        <CardHeader>
+          <CardTitle className="text-lg">{item.name}</CardTitle>
+          <CardDescription className="text-sm">{item.description}</CardDescription>
+        </CardHeader>
+      </Card>
+    </Link>
+  ))}
+</div>
