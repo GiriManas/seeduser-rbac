23119b160c1f121b24dfa3de70e78339084fb203
GiriManas <giri.manas007@gmail.com>
2025-04-02 19:08:53 +0530

    Update AdminRashboard.tsx

--- AdminRashboard.tsx
+++ AdminRashboard.tsx
@@ -9,12 +9,13 @@ const AdminDashboard = () => {
 
       {/* Grid Layout */}
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6 w-full max-w-4xl mx-auto justify-center">
+        
         {/* User Controls Card */}
-        <Card className="p-6 w-full h-44 flex flex-col justify-between shadow-lg hover:shadow-xl transition duration-300 text-center">
+        <Card className="p-6 w-full h-48 flex flex-col justify-between shadow-lg hover:shadow-xl transition duration-300 text-center">
           <CardHeader>
             <CardTitle className="text-lg font-semibold">User Controls</CardTitle>
           </CardHeader>
-          <CardContent className="flex flex-col items-center space-y-2">
+          <CardContent className="flex flex-col items-center justify-center flex-grow space-y-2 overflow-hidden">
             <Link href="/admin/users" className="text-blue-500 font-medium hover:underline">
               Active Users
             </Link>
@@ -28,11 +29,11 @@ const AdminDashboard = () => {
         </Card>
 
         {/* Roles Card */}
-        <Card className="p-6 w-full h-44 flex flex-col justify-between shadow-lg hover:shadow-xl transition duration-300 text-center">
+        <Card className="p-6 w-full h-48 flex flex-col justify-between shadow-lg hover:shadow-xl transition duration-300 text-center">
           <CardHeader>
             <CardTitle className="text-lg font-semibold">Roles</CardTitle>
           </CardHeader>
-          <CardContent className="flex flex-col items-center justify-center">
+          <CardContent className="flex flex-col items-center justify-center flex-grow">
             <Link href="/admin/roles" className="text-blue-500 font-medium hover:underline">
               Manage Roles
             </Link>
@@ -40,16 +41,17 @@ const AdminDashboard = () => {
         </Card>
 
         {/* Groups Card */}
-        <Card className="p-6 w-full h-44 flex flex-col justify-between shadow-lg hover:shadow-xl transition duration-300 text-center">
+        <Card className="p-6 w-full h-48 flex flex-col justify-between shadow-lg hover:shadow-xl transition duration-300 text-center">
           <CardHeader>
             <CardTitle className="text-lg font-semibold">Groups</CardTitle>
           </CardHeader>
-          <CardContent className="flex flex-col items-center justify-center">
+          <CardContent className="flex flex-col items-center justify-center flex-grow">
             <Link href="/admin/groups" className="text-blue-500 font-medium hover:underline">
               Manage Groups
             </Link>
           </CardContent>
         </Card>
+
       </div>
     </div>
   );
