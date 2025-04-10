885eaa6ba5279cad9e10b186309fa07e96bdaa55
GiriManas <giri.manas007@gmail.com>
2025-04-10 16:44:17 +0530

    Update 00001.tsx

--- 00001.tsx
+++ 00001.tsx
@@ -1,87 +1,17 @@
-function addRole(user: ActiveUser, newRole: string) {
-  const existingRoles = user.roles ?? [];
-  const updatedRoles = existingRoles.includes(newRole)
-    ? existingRoles
-    : [...existingRoles, newRole];
-
-  const updatedGroups = user.groups ?? [];
-
-  modifyAdminActiveUser(session.accessToken, user.user_id, {
-    roles: updatedRoles,
-    groups: updatedGroups,
-    au: user.au ?? "",
-  }).then(() => {
-    setUsers((prevUsers) =>
-      prevUsers.map((u) =>
-        u.user_id === user.user_id
-          ? { ...u, roles: updatedRoles }
-          : u
-      )
-    );
-  });
-}
-
-function removeRole(user: ActiveUser, roleToRemove: string) {
-  const existingRoles = user.roles ?? [];
-  const updatedRoles = existingRoles.filter(role => role !== roleToRemove);
-  const updatedGroups = user.groups ?? [];
-
-  modifyAdminActiveUser(session.accessToken, user.user_id, {
-    roles: updatedRoles,
-    groups: updatedGroups,
-    au: user.au ?? "",
-  }).then(() => {
-    setUsers((prevUsers) =>
-      prevUsers.map((u) =>
-        u.user_id === user.user_id
-          ? { ...u, roles: updatedRoles }
-          : u
-      )
-    );
-  });
-}
-
-
-function addGroup(user: ActiveUser, newGroup: string) {
-  const existingGroups = user.groups ?? [];
-  const updatedGroups = existingGroups.includes(newGroup)
-    ? existingGroups
-    : [...existingGroups, newGroup];
-
-  const updatedRoles = user.roles ?? [];
-
-  modifyAdminActiveUser(session.accessToken, user.user_id, {
-    roles: updatedRoles,
-    groups: updatedGroups,
-    au: user.au ?? "",
-  }).then(() => {
-    setUsers((prevUsers) =>
-      prevUsers.map((u) =>
-        u.user_id === user.user_id
-          ? { ...u, groups: updatedGroups }
-          : u
-      )
-    );
-  });
-}
-
-function removeGroup(user: ActiveUser, groupToRemove: string) {
-  const existingGroups = user.groups ?? [];
-  const updatedGroups = existingGroups.filter(group => group !== groupToRemove);
-  const updatedRoles = user.roles ?? [];
-
-  modifyAdminActiveUser(session.accessToken, user.user_id, {
-    roles: updatedRoles,
-    groups: updatedGroups,
-    au: user.au ?? "",
-  }).then(() => {
-    setUsers((prevUsers) =>
-      prevUsers.map((u) =>
-        u.user_id === user.user_id
-          ? { ...u, groups: updatedGroups }
-          : u
-      )
-    );
-  });
-}
-
+modifyAdminActiveUser(session.accessToken, user, {
+  role: updatedRoles,
+  group: updatedGroups,
+  au: user.au ?? "",
+}).then(() => {
+  const updatedUser = { ...user, roles: updatedRoles };
+  setUsers((prevUsers) =>
+    prevUsers.map((u) =>
+      u.user_id === user.user_id ? updatedUser : u
+    )
+  );
+  setUserList((prevList) =>
+    prevList.map((u) =>
+      u.user_id === user.user_id ? updatedUser : u
+    )
+  );
+});
