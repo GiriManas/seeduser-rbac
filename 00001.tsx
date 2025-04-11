7e90772ee9ad6217e864acbe3856968c4606c9e8
GiriManas <giri.manas007@gmail.com>
2025-04-11 11:33:55 +0530

    Update 00001.tsx

--- 00001.tsx
+++ 00001.tsx
@@ -1,17 +1,18 @@
-modifyAdminActiveUser(session.accessToken, user, {
-  role: updatedRoles,
-  group: updatedGroups,
-  au: user.au ?? "",
-}).then(() => {
-  const updatedUser = { ...user, roles: updatedRoles };
-  setUsers((prevUsers) =>
-    prevUsers.map((u) =>
-      u.user_id === user.user_id ? updatedUser : u
-    )
-  );
-  setUserList((prevList) =>
-    prevList.map((u) =>
-      u.user_id === user.user_id ? updatedUser : u
-    )
-  );
-});
+<Dialog open={isOpen} onOpenChange={onClose}>
+  <DialogContent className="max-h-[90vh] overflow-y-auto">
+    <DialogHeader>
+      <DialogTitle>Edit User</DialogTitle>
+    </DialogHeader>
+
+    <div className="space-y-2">
+      <div><strong>ID:</strong> {user.id}</div>
+      <div><strong>User ID:</strong> {user.user_id}</div>
+      <div><strong>Name:</strong> {user.name}</div>
+      <div><strong>Email:</strong> {user.email}</div>
+      <div><strong>Status:</strong> {user.status}</div>
+      <div><strong>Approver:</strong> {user.approver}</div>
+      <div><strong>Created:</strong> {user.created_at}</div>
+      <div><strong>Updated:</strong> {user.updated_at}</div>
+    </div>
+
+    {/* Existing Editable Fields follow... */}
