92a8435967bb8eed1ff18d9ac9521f2a272a4082
GiriManas <giri.manas007@gmail.com>
2025-05-21 11:23:05 +0530

    Update topic_clustering.py

--- topic_clustering.py
+++ topic_clustering.py
@@ -23,3 +23,31 @@ if isinstance(sentence_embedding, str):
         self.embedding_model = SentenceTransformer(sentence_embedding, device="cpu")
 else:
     self.embedding_model = sentence_embedding
+    
+    
+
+def load_LLM(self, model_path='/commons/cobra_share/VIPER_NLP/hf_model_hub/llama3-8b-instruct/'):
+    self.tokenizer = AutoTokenizer.from_pretrained(model_path)
+
+    model_loaded = False
+    if torch.cuda.is_available():
+        for i in range(torch.cuda.device_count()):
+            try:
+                device = f"cuda:{i}"
+                print(f"Trying LLM on {device}")
+                self.model = AutoModelForCausalLM.from_pretrained(model_path, torch_dtype=torch.float16).to(device)
+                _ = self.model.generate(**self.tokenizer("test", return_tensors="pt").to(device))  # test allocation
+                print(f"Model loaded on {device}")
+                model_loaded = True
+                break
+            except RuntimeError as e:
+                if "CUDA out of memory" in str(e):
+                    print(f"{device} out of memory. Trying next GPU...")
+                else:
+                    raise e
+
+    if not model_loaded:
+        print("Falling back to CPU.")
+        self.model = AutoModelForCausalLM.from_pretrained(model_path).to("cpu")
+
+    return
