51c372bdd980608217288b5cbc0f0423a8235dba
GiriManas <giri.manas007@gmail.com>
2025-07-23 21:00:57 +0530

    Create phi-model.py

---
+++ phi-model.py
@@ -0,0 +1,12 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+model_name = "microsoft/Phi-3-mini-128k-instruct"
+
+tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained(model_name, trust_remote_code=True, device_map="auto")
+
+prompt = "Explain gravity in simple terms."
+
+inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
+outputs = model.generate(**inputs, max_new_tokens=100)
+print(tokenizer.decode(outputs[0], skip_special_tokens=True))
