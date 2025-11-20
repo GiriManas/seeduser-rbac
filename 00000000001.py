49d0892fae38ffe6e1c9b673be00b5736420e73b
GiriManas <giri.manas007@gmail.com>
2025-11-20 13:48:01 +0530

    Update 00000000001.py

--- 00000000001.py
+++ 00000000001.py
@@ -1,6 +1,8 @@
+# main.py (top of the file)
+
 import os
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 os.environ["OMP_NUM_THREADS"] = "1"
 os.environ["MKL_NUM_THREADS"] = "1"
-
-from transformers import AutoTokenizer  # import AFTER
+os.environ["NUMEXPR_NUM_THREADS"] = "1"
+os.environ["HF_HUB_DISABLE_SYMLINKS_WARNING"] = "1"
