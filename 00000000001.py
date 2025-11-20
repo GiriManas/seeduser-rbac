a3caf95f7855b7cd5311abc690a179c659326287
GiriManas <giri.manas007@gmail.com>
2025-11-20 12:35:40 +0530

    Update 00000000001.py

--- 00000000001.py
+++ 00000000001.py
@@ -1,14 +1,6 @@
-#!/bin/bash
+import os
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
 
-echo "Enter your choice (1 or 2 or 9): "
-read user_input
-
-if [[ "$user_input" != "1" && "$user_input" != "2" && "$user_input" != "9" ]]; then
-    echo "Invalid input! Please enter 1, 2, or 9."
-    exit 1
-fi
-
-# Run the Python script with nohup and redirect logs
-nohup python3 run_wrapped_tokenizer.py "$user_input" > run.log 2>&1 &
-
-echo "Process started with input $user_input. Logs are in run.log"
+from transformers import AutoTokenizer  # import AFTER
