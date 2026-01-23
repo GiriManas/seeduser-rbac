62a300a40f6ffabf365e597683f9dbc3bad2d242
GiriManas <giri.manas007@gmail.com>
2026-01-23 07:13:43 +0530

    Update 00000000001.py

--- 00000000001.py
+++ 00000000001.py
@@ -1,21 +1,9 @@
-start_time = time.time()
-
-X_tensor = torch.tensor(
-    X_test_history.values,
-    device="cuda",
-    dtype=torch.float32
-)
-
-preds = []
-probs = []
 batch = 8192
+preds, probs = [], []
 
-for i in range(0, len(X_tensor), batch):
-    proba = clf.predict_proba(X_tensor[i:i+batch])
+for i in range(0, len(X_test_history), batch):
+    X_batch = X_test_history.iloc[i:i+batch]
+
+    proba = clf.predict_proba(X_batch)
     probs.append(proba[:, 1])
     preds.append((proba[:, 1] > 0.5).astype(int))
-
-y_pred = np.concatenate(preds)
-y_prob = np.concatenate(probs)
-
-print(f"Prediction time: {time.time() - start_time:.2f}s")
