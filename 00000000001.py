6b7d92530a0972d8c7404c972e08f3047f89aef7
GiriManas <giri.manas007@gmail.com>
2026-02-11 15:54:05 +0530

    Update 00000000001.py

--- 00000000001.py
+++ 00000000001.py
@@ -1,27 +1,41 @@
+import matplotlib.pyplot as plt
+
+# Create masks
+fraud_mask = labels_sub == 1
+nonfraud_mask = labels_sub == 0
+
+# Fix axis limits for fair comparison
 x_min, x_max = embeddings_2d[:,0].min(), embeddings_2d[:,0].max()
 y_min, y_max = embeddings_2d[:,1].min(), embeddings_2d[:,1].max()
 
 plt.figure(figsize=(14,6))
 
+# --- Non-Fraud ---
 plt.subplot(1, 2, 1)
-plt.scatter(embeddings_2d[nonfraud_mask, 0],
-            embeddings_2d[nonfraud_mask, 1],
-            s=5, alpha=0.6)
+plt.scatter(
+    embeddings_2d[nonfraud_mask, 0],
+    embeddings_2d[nonfraud_mask, 1],
+    s=5,
+    alpha=0.6
+)
 plt.xlim(x_min, x_max)
 plt.ylim(y_min, y_max)
 plt.title("Non-Fraud")
 
+# --- Fraud ---
 plt.subplot(1, 2, 2)
-plt.scatter(embeddings_2d[fraud_mask, 0],
-            embeddings_2d[fraud_mask, 1],
-            s=5, alpha=0.6)
+plt.scatter(
+    embeddings_2d[fraud_mask, 0],
+    embeddings_2d[fraud_mask, 1],
+    s=5,
+    alpha=0.6
+)
 plt.xlim(x_min, x_max)
 plt.ylim(y_min, y_max)
 plt.title("Fraud")
 
 plt.tight_layout()
 plt.show()
-
 
 
 
